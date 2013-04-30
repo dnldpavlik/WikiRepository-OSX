78c38e87aba07553cb07003bd7bc64cc8d24e389
Don Pavlik <git@donpavlik.net>
2013-04-29 23:24:06 -0400

    Addition of wiki file protocol

---
+++ WikiRepository/IWikiFile.h
@@ -0,0 +1,32 @@
+//
+//  IWikiFile.h
+//  WikiRepository
+//
+//  Created by Donald Pavlik on 4/29/13.
+//  Copyright (c) 2013 Donald Pavlik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol IWikiFile <NSObject>
+
+/*!
+    Title of the Wiki File
+    @result Gets the Title of the file
+*/
+@property (string, nonatomic) NSString *title;
+
+/*!
+    Meta Data for the Wiki File
+    @result Dictionary with collection of available meta data
+    @discussion One item stored in here is the actual file name of the wiki file
+*/
+@property (strong, nonatomic, readonly) NSMutableDictonary *metaData;
+
+/*!
+    Content of the wiki file in an array of lines, each line is a NSString
+    @result Array of string of the contents of the file
+*/
+@property (strong, nonatomic, readonly) NSMutableArray *lines;
+
+@end
